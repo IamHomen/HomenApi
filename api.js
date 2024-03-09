3ed0b012222330822f4d1e0957f3cbdf30de3bd3
MrCain <50195844+mrcainv1-3128@users.noreply.github.com>
2024-03-09 13:21:50 +0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+import {
+scrapeM3U8,
+} from './episode_parser.js';
+
+const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: '*',
+    credentails: true,
+    optionSuccessStatus: 200,
+    port: port,
+};
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome to GogoAnime API by Homen!');
+});
+
+app.get('/vidcdn/watch/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+
+        const data = await scrapeM3U8({ id: id });
+
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({
+            status: 500,
+            error: 'Internal Error',
+            message: err,
+        });
+    }
+});
+
+app.use((req, res) => {
+    res.status(404).json({
+        status: 404,
+        error: 'Not Found',
+    });
+});
+
+app.listen(port, () => {
+    console.log('Express server listening on port %d in %s mode', port, app.settings.env);
+});
---
+++ episode_parser.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+import {
+ generateEncryptAjaxParameters,
+ decryptEncryptAjaxResponse,
+} from './goload.js';
+
+import { USER_AGENT, renameKey } from './utils.js';
+
+const Referer = 'https://gogoplay.io/';
+const goload_stream_url = 'https://goload.pro/streaming.php';
+
+const BASE_URL2 = 'https://anitaku.to/';
+
+export const scrapeM3U8 = async ({ id }) => {
+ let sources = [];
+ let sources_bk = [];
+ try {
+  let epPage, server, $, serverUrl;
+
+  if (id) {
+   epPage = await axios.get(BASE_URL2 + id);
+   $ = cheerio.load(epPage.data);
+
+   server = $('#load_anime > div > div > iframe').attr('src');
+   serverUrl = new URL(server);
+  } else throw Error('Episode id not found');
+
+  const goGoServerPage = await axios.get(serverUrl.href, {
+   headers: { 'User-Agent': USER_AGENT },
+  });
+  const $$ = cheerio.load(goGoServerPage.data);
+
+  const params = await generateEncryptAjaxParameters(
+   $$,
+   serverUrl.searchParams.get('id')
+  );
+
+  const fetchRes = await axios.get(
+   `
+        ${serverUrl.protocol}//${serverUrl.hostname}/encrypt-ajax.php?${params}`,
+   {
+    headers: {
+     'User-Agent': USER_AGENT,
+     'X-Requested-With': 'XMLHttpRequest',
+    },
+   }
+  );
+
+  const res = decryptEncryptAjaxResponse(fetchRes.data);
+
+  if (!res.source) return { error: 'No sources found!! Try different source.' };
+
+  res.source.forEach((source) => sources.push(source));
+  res.source_bk.forEach((source) => sources_bk.push(source));
+
+  return {
+   Referer: serverUrl.href,
+   sources: sources,
+   sources_bk: sources_bk,
+  };
+ } catch (err) {
+  return { error: err };
+ }
+};
